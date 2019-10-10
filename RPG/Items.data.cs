5bda60aa448a71a641ee38ba458e617f3ba8c596
benjilebon <benji.lebon467@gmail.com>
2019-10-10 15:18:23 +0200

    Added Items for Lvl3 and Lvl4

--- RPG/Items.data.cs
+++ RPG/Items.data.cs
@@ -13,6 +13,14 @@ namespace RPG
             new Item(3, 1, 50, 2, "Elixir de soin", "Une potion pour soigner vos blessures", 15),
             new Item(4, 3, 15, 2, "Sabre vicié", "Un sabre pour trancher vos ennemis", 25),
             new Item(5, 2, 15, 2, "Cotte de mailles", "Une armure lourde pour parer un nombre conséquents de dégats", 50),
+
+            new Item(6, 4, 25, 3, "Bracelet Cristallin", "Un bracelet renforcé en cristal augmentant vos HP", 30),
+            new Item(7, 3, 35, 3, "BF Glaive", "Un redoutable Glaive pour trancher vos adversaires", 50),
+            new Item(8, 2, 30, 3, "Cotte épineuse", "Puissante armure contre les plus gros adversaires", 80),
+
+            new Item(8, 4, 100, 4, "Warmog", "Une armure vivant offrant une grande vitalité", 100),
+            new Item(6, 1, 200, 4, "Rédemption", "Bénédiction divine soignant une grande quantité d'hp", 45),
+            new Item(7, 3, 50, 4, "BF Glaive", "Un redoutable Glaive pour trancher vos adversaires", 60),
         };
         
         public ItemsData()
